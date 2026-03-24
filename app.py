06371a657cef780f3af1489c8e37218191ab5a9f
n-manohar007 <manoharnellikondi1@gmail.com>
2026-03-24 12:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,8 @@ def profile_route():
 def logout_route():
     return logout()
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
